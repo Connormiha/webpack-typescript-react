140d86951aa4c09f88359f0c9c4bbb12e59f0bb5
Connormiha <connormiha1@gmail.com>
2016-01-01 04:45:12 +0300

    pre-commit check only changed files

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,27 @@
 const gulp = require('gulp');
 const tslint = require('gulp-tslint');
 const stylint = require('gulp-stylint');
+const execSync = require('child_process').execSync;
+
+const PRE_COMMIT = process.env.NODE_ENV === 'pre_commit';
+
+let changedFiles;
+let changedFilesTypescript;
+let changedFilesStylus;
+
+if (PRE_COMMIT) {
+    changedFiles = execSync('git diff --cached --name-only --diff-filter=ACM', {encoding: 'utf8'});
+    changedFiles = changedFiles.split('\n');
+    changedFilesTypescript = changedFiles.filter((item) => /\.tsx?$/.test(item));
+    changedFilesStylus = changedFiles.filter((item) => /\.styl$/.test(item));
+}
 
 /**
  *
  * @desc Check TypeScript validation
  */
 gulp.task('tslint', ()=>
-    gulp.src(['src/**/*.{ts,tsx}'])
+    gulp.src(PRE_COMMIT ? changedFilesTypescript : ['src/**/*.{ts,tsx}'])
         .pipe(tslint())
         .pipe(tslint.report('verbose'))
 );
@@ -19,7 +33,7 @@ gulp.task('tslint', ()=>
  * @desc Check stylus validation
  */
 gulp.task('styluslint', ()=>
-    gulp.src(['src/css/*.styl'])
+    gulp.src(PRE_COMMIT ? changedFilesStylus : ['src/css/*.styl'])
         .pipe(stylint())
         .pipe(stylint.reporter())
 );
