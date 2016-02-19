dadd86cb6b26dacddedc1285d7851a1688338a03
Connormiha <connormiha1@gmail.com>
2016-02-20 01:53:23 +0300

    add react-router

--- src/ts/view.tsx
+++ src/ts/view.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import * as Reflux from 'reflux';
 import * as Store from './store';
+import * as ReactRouter from 'react-router';
 
 const STORE_MIXINS = Reflux.connect(Store.eventStore, 'list');
 
@@ -51,6 +52,54 @@ export class BtnGenerate extends React.Component<any, any> {
 	render () {
 		return (
 			<input type='button' value={this.props.value} onClick={this.handleClick.bind(this)} />
+		);
+	}
+};
+
+export class About extends React.Component<any, any> {
+	render () {
+		return (
+			<div>
+				<p>This is Started development kit for Single Page Application.</p>
+				<p>Using stack technologies:</p>
+				<ul>
+					<li>React</li>
+					<li>React-router</li>
+					<li>Typescript</li>
+					<li>Webpack</li>
+					<li>Stylus</li>
+				</ul>
+				<ReactRouter.IndexLink to="/">To main page</ReactRouter.IndexLink>
+			</div>
+		);
+	}
+};
+
+export class GeneratorApp extends React.Component<any, any> {
+	render () {
+		return (
+			<section id="generator-app">
+				<section id="content">
+					<ItemsList />
+				</section>
+				<div id="buttons">
+					<BtnGenerate value='Regenerate list' />
+				</div>
+				<ReactRouter.Link to="/about">About this project</ReactRouter.Link>
+			</section>
+		);
+	}
+};
+
+export class App extends React.Component<any, any> {
+	render () {
+		return (
+			<div id="app">
+				<div id="app-name">
+					<AppName name='Empty Typescript+React project' />
+				</div>
+				{this.props.children}
+			</div>
 		);
 	}
 };
